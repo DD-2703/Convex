8ca196adc2cd75f921106e721b7affd8d2cfebb6
DDane <82321835+DD-2703@users.noreply.github.com>
2023-03-03 18:54:01 +0530

    Fixed Logging and added colors to logging module

--- Logging/logging.py
+++ Logging/logging.py
@@ -2,37 +2,67 @@
 Contains all logging-related functions.
 '''
 
-__log_level: int = 0
+_private_log_level: int = 0
 
 def Log(variable):
     print("[DEBUG]      "+variable)
 
-def Info(msg: str, level_matters=False):
+def PInfo(msg: str, level_matters:bool=False):
     if level_matters:
-        if __log_level==0:
+        if _private_log_level==0:
             print("[INFO]       "+msg)
             return
         else: return
     print("[INFO]       "+msg)
 
-def Warn(msg: str, level_matters=False):
+def PWarn(msg: str, level_matters:bool=False):
     if level_matters:
-        if __log_level<=1:
+        if globals()['_private_log_level']<=1:
             print("[WARNING]    "+msg)
-            __log_level = 1
+            globals()['_private_log_level'] = 1
             return
         else: return
     print("[WARNING]    "+msg)
 
-def Exception(msg: str, level_matters=False):
+def PException(msg: str, level_matters:bool=False):
     if level_matters:
-        if __log_level<=2:
+        if globals()['_private_log_level']<=2:
             print("[EXCEPTION]  "+msg)
-            __log_level = 2
+            globals()['_private_log_level'] = 2
             return
         else: return
     print("[EXCEPTION]  "+msg)
 
-def Critical(msg: str):
+def PCritical(msg: str):
     print("[CRITICAL]   "+msg)
+    exit(1)
+
+def Info(msg: str, level_matters:bool=False):
+    if level_matters:
+        if _private_log_level==0:
+            print("\033[92m[INFO]       "+msg+'\033[0m')
+            return
+        else: return
+    print("\033[92m[INFO]       "+msg+'\033[0m')
+
+def Warn(msg: str, level_matters:bool=False):
+    if level_matters:
+        if globals()['_private_log_level']<=1:
+            print("\033[93m[WARNING]    "+msg+'\033[0m')
+            globals()['_private_log_level'] = 1
+            return
+        else: return
+    print("\033[93m[WARNING]    "+msg+'\033[0m')
+
+def Exception(msg: str, level_matters:bool=False):
+    if level_matters:
+        if globals()['_private_log_level']<=2:
+            print("\033[91m[EXCEPTION]  "+msg+'\033[0m')
+            globals()['_private_log_level'] = 2
+            return
+        else: return
+    print("\033[91m[EXCEPTION]  "+msg+'\033[0m')
+
+def Critical(msg: str):
+    print("\033[91m\033[1m[CRITICAL]   "+msg+'\033[0m\033[0m')
     exit(1)
